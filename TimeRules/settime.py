aa6a3a7faa27b5c4622c77074a4bb27e9af44f7f
fahiran007 <fahiranahmed@gmail.com>
2022-10-05 12:00:21 +0530

    initial commit

--- TimeRules/settime.py
+++ TimeRules/settime.py
@@ -17,17 +17,17 @@ def serialNum(collage,writting):
     # collage no, writting no
     if collage == "no" and writting == "no":
         l2 = [datetime.time(5, 0), datetime.time(5, 20), datetime.time(5, 40), datetime.time(5, 50), datetime.time(7, 5), datetime.time(9, 0), datetime.time(9, 10), datetime.time(10, 0), datetime.time(13, 0), datetime.time(14, 0), datetime.time(14, 40), datetime.time(15, 40), datetime.time(16, 50), datetime.time(17, 55), datetime.time(19, 50), datetime.time(21, 30), datetime.time(21, 40), datetime.time(22, 0)]
-        num = serialFinal(l1)
+        num = serialFinal(l2)
 
     # collage yes, writting yes
     if collage == "yes" and writting == "yes":
         l3 = [datetime.time(5, 0), datetime.time(5, 20), datetime.time(5, 40), datetime.time(5, 50), datetime.time(7, 5), datetime.time(9, 0), datetime.time(9, 10), datetime.time(9, 25), datetime.time(15, 10), datetime.time(15, 50), datetime.time(17, 30), datetime.time(17, 55), datetime.time(19, 50), datetime.time(21, 30), datetime.time(21, 40), datetime.time(22, 0)]
-        num = serialFinal(l1)
+        num = serialFinal(l3)
 
     # collage yes, writting no
     if collage == "yes" and writting == "no":
         l4 = [datetime.time(5, 0), datetime.time(5, 20), datetime.time(5, 40), datetime.time(5, 50), datetime.time(7, 5), datetime.time(9, 0), datetime.time(9, 10), datetime.time(9, 25), datetime.time(15, 10), datetime.time(15, 50), datetime.time(17, 0), datetime.time(17, 55), datetime.time(19, 50), datetime.time(21, 30), datetime.time(21, 40), datetime.time(22, 0)]
-        num = serialFinal(l1)
+        num = serialFinal(l4)
     return num
 def serialFinal(l):
     time = times()
@@ -35,4 +35,5 @@ def serialFinal(l):
     for i in range(len(l)):
         if l[i] <= time:
             serial = serial + 1
+            print(serial)
     return serial
